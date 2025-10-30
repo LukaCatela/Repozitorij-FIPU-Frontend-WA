7ba43c016a9d662ff4ae1bbde6c067e4926b1f22
Luka Catela <luca.catela8@gmail.com>
2025-10-30 19:30:22 +0100

    Inicijaliziran frontend project

--- FIPUHub/src/App.vue
+++ FIPUHub/src/App.vue
@@ -1,11 +1,14 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="
+text-lime-300
+bg-zinc-800/90
+border-sky-700
+border-2
+">
+Some text
+</div>
 </template>
 
 <style scoped></style>
