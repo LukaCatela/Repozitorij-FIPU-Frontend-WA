08f23d9de5696befe74f81a60fab8532e1372d48
Luka Catela <luca.catela8@gmail.com>
2026-03-20 12:17:13 +0100

    Kada korisnik nije registriran da nema pristup

--- FIPUHub/src/views/Homepage.vue
+++ FIPUHub/src/views/Homepage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="min-h-screen bg-white text-gray-900">
-    <!-- HERO -->
     <section class="px-6 lg:px-20 py-20 flex flex-col lg:flex-row items-center gap-10">
       <div class="flex-1">
         <h1 class="text-4xl lg:text-5xl font-bold leading-tight">
-          Dobrodošli u <span class="text-[#FFB703]">FIPU Hub</span>
+          Dobrodošli u <span class="text-[#8ECAE6]">FIPU</span
+          ><span class="text-[#FFB703]">Hub</span>
         </h1>
         <p class="mt-5 text-lg text-[#219EBC] max-w-xl">
           Centralno mjesto za studentske projekte, članke, znanstvene radove i razmjenu poslova.
@@ -12,19 +12,36 @@
         </p>
 
         <div class="flex gap-4 mt-8">
-          <RouterLink
-            to="/explore"
-            class="bg-[#FFB703] text-[#023047] px-5 py-3 rounded-xl shadow-md hover:bg-[#FB8500] transition"
-          >
-            Pregledaj radove
-          </RouterLink>
+          <template v-if="auth.isLoggedIn">
+            <RouterLink
+              to="/explore"
+              class="bg-[#FFB703] text-[#023047] px-5 py-3 rounded-xl shadow-md hover:bg-[#FB8500] transition"
+            >
+              Pregledaj radove
+            </RouterLink>
 
-          <RouterLink
-            to="/post-job"
-            class="bg-white border border-gray-300 px-5 py-3 rounded-xl shadow hover:bg-[#FFB703] transition"
-          >
-            Objavi oglas
-          </RouterLink>
+            <RouterLink
+              to="/post-job"
+              class="bg-white border border-gray-300 px-5 py-3 rounded-xl shadow hover:bg-[#FFB703] transition"
+            >
+              Objavi oglas
+            </RouterLink>
+          </template>
+          <template v-else="!auth.isLoggedIn">
+            <RouterLink
+              to="/register"
+              class="bg-[#FFB703] text-[#023047] px-5 py-3 rounded-xl shadow-md hover:bg-[#FB8500] transition"
+            >
+              Pregledaj radove
+            </RouterLink>
+
+            <RouterLink
+              to="/register"
+              class="bg-white border border-gray-300 px-5 py-3 rounded-xl shadow hover:bg-[#FFB703] transition"
+            >
+              Objavi oglas
+            </RouterLink>
+          </template>
         </div>
       </div>
 
@@ -33,7 +50,6 @@
       </div>
     </section>
 
-    <!-- FEATURES -->
     <section class="bg-white py-16 px-6 lg:px-20">
       <h2 class="text-3xl font-bold text-center mb-12">Što nudi FIPU Hub?</h2>
 
@@ -61,4 +77,8 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
+</script>
