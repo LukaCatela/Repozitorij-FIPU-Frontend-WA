922f8db4312eeaa0be6a482ffb6079449f8b8dd0
Luka Catela <luca.catela8@gmail.com>
2026-03-24 12:46:32 +0100

    dodani novi view-s

--- FIPUHub/src/App.vue
+++ FIPUHub/src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
-import Body from './components/Body.vue';
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+import Body from './components/Body.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import { onMounted } from 'vue'
+import { useAuthStore } from './stores/auth.js'
+
+const auth = useAuthStore()
+
+onMounted(async () => {
+  if (localStorage.getItem('token')) {
+    await auth.fetchMe()
+  }
+})
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <Header/>
+    <Header />
 
     <div class="flex-1 flex justify-center">
-      <Body/>
+      <Body />
     </div>
 
-  <Footer/>
+    <Footer />
   </div>
 </template>
